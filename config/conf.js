9f7e429cb925495ca719a79fc707f4cb32a4517b
Stuart Young <stuart@goba.mobi>
2013-10-01 14:28:41 -0500

    Updated nginx config and conf.js with object model.

--- config/conf.js
+++ config/conf.js
@@ -44,14 +44,85 @@ module.exports.hSettings = {
         }
         ,hClasses:{
             User:{
-                aProperties:[]
+                aProperties:['sID','sFirstName','sLastName','sPassword','sEmail','nBirthday','nCreated','nUpdated']
+                ,hExtras:{
+                    cFriends:{
+                        sType:'Collection'
+                        ,sClass:'User'
+                        ,fnQuery:function(){
+
+                        }
+                    }
+                    ,cTiles:{
+                        sType:'Collection'
+                        ,sClass:'Tile'
+                        ,sSortBy:'nUpdated'
+                        ,bReverse:'true'
+                        ,fnQuery:function(oObj,App){
+                            return {nUserID:oObj.get('nID')}
+                        }
+                    }
+
+                }
+            }
+            ,Tile:{
+                aProperties:['sID','nUserID','nObjectClass','nObjectID','nType','nCreated','nUpdated']
+                ,nType:'Public or private'
+                ,cComments:{
+                    sType:'Collection'
+                    ,sClass:'Comment'
+                    ,fnQuery:function(oObj,App){
+                        return {nObjectID:oObj.get('nID')}
+                    }
+                }
+            }
+            ,Comment:{
+                aProperties:['sID','nUserID','sMessage','nCreated','nUpdated']
+                ,hExtras:{
+                    cCopies:{
+                        sType:'Collection'
+                        ,sClass:'CarbonCopy'
+                        ,fnQuery:function(oObj,App){
+                            return {nCommentID:oObj.get('nID')}
+                        }
+                    }
+                }
+            }
+            ,Post:{
+                aProperties:['sID','sMessage','nCreated','nUpdated']
+            }
+            ,Visit:{
+                aProperties:['sID','nDate','sProvider','sLocation','sNotes']
+            }
+            ,Doctor:{}
+            ,Provider:{}
+            ,ContentProvider:{}
+            ,Article:{
+                aProperties:['sID','sImage','sTitle','sBody','sUrl','nContentProviderID','nCreated','nUpdated']
+            }
+            ,Medication:{
+                aProperties:['sID','sTitle','sDosage','sFrequency','sProvider','sNotes']
+            }
+            ,Supplement:{
+                aProperties:['sID','sTitle','sDosage','sFrequency','sProvider','sNotes']
+            }
+            ,CarbonCopy:{
+                aProperties:['sID','nUserID','nCommentID']
             }
         }
         ,hClassMap:{
-            1:'User'
+            1:'User',2:'Tile',3:'Comment',4:'Post',5:'Visit',6:'Article',7:'Medication',8:'Supplement',9:'CarbonCopy'
         }
         ,hAPIHelper:{
             user:'User'
+            ,tile:'Tile'
+            ,comment:'Comment'
+            ,post:'Post'
+            ,visit:'Visit'
+            ,article:'Article'
+            ,medication:'Medication'
+            ,supplement:'Supplement'
+            ,'carboncopy':'CarbonCopy'
         }
     }
 };

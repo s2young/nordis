bd1850a9f87d17109ecc9a1ede09386cc43a6746
Stuart Young <stuart@goba.mobi>
2013-10-01 10:00:04 -0500

    First deposit of core code, utils and cofig.

---
+++ conf/conf.js
@@ -0,0 +1,58 @@
+module.exports.hSettings = {
+    global: {
+        nStartingID:1000000
+        ,aEmergencyEmails:['stuart@gobaengage.com']
+        ,hConstants:{
+            sLogLevel:'debug',
+            sServerTZ:'America/Chicago'
+        }
+        ,hOptions:{
+            MySql:{
+                sSchema:'nordis_dev',
+                sHost:'10.224.5.252',
+                sUser:'root',
+                sPass:'zat{blos0Fru',
+                nMaxConnections:2000,
+                nTimeoutMilliseconds:10000,
+                bDebugMode:false
+            },
+            Redis:{
+                sWriteServer:'127.0.0.1',
+                nWritePort:6379,
+                nDb:2,
+                nMaxConnections:2000,
+                nTimeoutMilliseconds:30000,
+                nReapIntervalMilliseconds:5000,
+                bDebugMode:false
+            },
+            Email:{
+                oQuickMail:{
+                    host: 'smtp.gmail.com',
+                    secureConnection: true,
+                    port: 465,
+                    auth: {
+                        user: 's2.d.young@gmail.com',
+                        pass: 'L3xLuth3r_g'
+                    }
+                }
+            }
+        }
+        ,hAppSettings:{
+            agewize:{
+                nPort:3000,
+                nSessionLength:864000
+            }
+        }
+        ,hClasses:{
+            User:{
+                aProperties:[]
+            }
+        }
+        ,hClassMap:{
+            1:'User'
+        }
+        ,hAPIHelper:{
+            user:'User'
+        }
+    }
+};

1bd8faf91bd16891260860906cb7ce5c006de6d1
Stuart Young <stuart@goba.mobi>
2013-10-01 20:58:29 -0500

    Consolidating nordis framework into library for re-use.

--- config/servers/joyent/dev/conf.js
+++
@@ -1,7 +0,0 @@
-module.exports.hSettings = {
-    'dev':{
-	    hConstants:{
-			sLogLevel:"debug"
-        }
-	}
-};
---
+++ index.js
@@ -0,0 +1,3 @@
+module.exports.AppConfig = require('./lib/AppConfig');
+module.exports.Base = require('./lib/Base');
+module.exports.Collection = require('./lib/Collection');

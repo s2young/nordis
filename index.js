435b218e508342b0f6248d7891449d0adfead012
Stuart Young <stuart@goba.mobi>
2013-10-02 12:40:32 -0500

    Exposing String utilities to the world, specifically for access to isValidEmail method.

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ module.exports.AppConfig = require('./lib/AppConfig');
 module.exports.Base = require('./lib/Base');
 module.exports.Collection = require('./lib/Collection');
 module.exports.Template = require('./lib/Utils/Template');
+module.exports.Str = require('./lib/Utils/String');

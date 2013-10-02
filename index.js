1271ff8dc088aa604ca23bbd7b25c49cab752dc5
Stuart Young <stuart@goba.mobi>
2013-10-02 00:09:52 -0500

    Exposing doT via Template class in nordis.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 module.exports.AppConfig = require('./lib/AppConfig');
 module.exports.Base = require('./lib/Base');
 module.exports.Collection = require('./lib/Collection');
+module.exports.Template = require('./lib/Utils/Template');

317507fed56d9544ef35c3f42b199489e3b31be1
Stuart Young <stuart@goba.mobi>
2013-11-13 10:10:28 -0600

    Export middleware via index.js.

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ module.exports.AppConfig = require('./lib/AppConfig');
 module.exports.Base = require('./lib/Base');
 module.exports.Collection = require('./lib/Collection');
 module.exports.Template = require('./lib/Utils/Template');
+module.exports.Middleware = require('./lib/Utils/Middleware');
 module.exports.Str = require('./lib/Utils/String');

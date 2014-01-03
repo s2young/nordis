9d744cfb7581158d2ae6e960d8773f75f2477d87
Stuart Young <stuart@goba.mobi>
2014-01-03 14:30:02 -0600

    Console logging on App.debug calls to see why agewize isn't logging at proper level.

--- example/conf.js
+++ example/conf.js
@@ -7,7 +7,7 @@ module.exports.hSettings = {
         nSeedID:1000000
         ,sLanguage:'en'
         ,aEmergencyEmails:['s2.d.young@gmail.com']
-        ,sLogLevel:'debug'
+        ,sLogLevel:'warn'
         ,hOptions:{
             MySql:{
                 sSchema:'nordis',
